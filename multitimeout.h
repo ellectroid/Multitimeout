6c3d13a84c003d7ab3ebdd4c4d1214d96c2b97d4
Ilya <72581491+ellectroid@users.noreply.github.com>
2024-01-30 14:34:39 +0100

    Update multitimeout.h
    Added custom parameters for handler, because reality test of this library found it was a useful missing piece.

--- multitimeout.h
+++ multitimeout.h
@@ -10,24 +10,26 @@ class Multitimeout{
 public:
 	typedef std::chrono::steady_clock::time_point timepoint_t;
 	typedef std::chrono::duration<int, std::milli> ms_t;
-	typedef void (*timeout_handler_func_t)(Multitimeout* Multitimeout, int timer_id); //void func(Multitimeout* this, int timer_id)
+	typedef void (*timeout_handler_func_t)(Multitimeout* Multitimeout, int timer_id, void* arg_ptr, int arg_int); //void func(Multitimeout* this, int timer_id)
 private:
 	//threads
 	std::thread timeout_thread_;
 	std::mutex object_access_mutex_;
-	std::counting_semaphore<16> timeout_sem_;
+	std::counting_semaphore<32767> timeout_sem_;
 	ms_t sleep_time_;
 	timepoint_t reference_timepoint_;
 	volatile bool thread_ready_;
 	int thread_signal_;
 	timeout_handler_func_t timeout_event_handler_;
+	void* handler_arg_ptr_;
+	int handler_arg_int_;
 
-	ms_t timeout_[16];
-	ms_t timeout_reload_[16];
-	bool timeout_active_[16];
-	bool timeout_expired_[16];
-	bool timeout_repeat_[16];
-	bool timeout_update_skip_[16];
+	ms_t* timeout_;
+	ms_t* timeout_reload_;
+	bool* timeout_active_;
+	bool* timeout_expired_;
+	bool* timeout_repeat_;
+	bool* timeout_update_skip_;
 	int timeout_count_;
 	int timeout_active_count_;
 	int timeout_expired_counter_;
@@ -45,6 +47,8 @@ public:
 	int remove(int timer_id);
 	int bind_event_handler(timeout_handler_func_t handlerfunc);
 	int unbind_event_handler();
+	int bind_memory(int timeout_count, ms_t* timeout_mem, ms_t* timeout_reload_mem, bool* timeout_active_mem, bool* timeout_expired_mem, bool* timeout_repeat_mem, bool* timeout_update_skip_mem);
+	int bind_handler_arguments(void* arg_ptr, int arg_int);
 };
 
 
